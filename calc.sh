162e3949cab30c7c5451466c7f986ad308ce937c
Monooks <124846515+Monooks@users.noreply.github.com>
2023-02-28 17:27:20 +0700

    Update calc.sh

--- calc.sh
+++ calc.sh
@@ -1,18 +1,30 @@
 #!/bin/bash
 while true; do
-    read -p "Say me the first number?" a
-    read -p "Say me the second number?" b
-    PS3="What kind of operation you want?"
-    select ans in add subtract multiply divide; do
-        case $ans in
-            add) op='+' ; break ;;
-            subtract) op='-' ; break ;;
-            multiply) op='*' ; break ;;
-            divide) op='/' ; break ;;
-            *) echo "invalid response" ;;
-        esac
-    done
-    ans=$(echo "$a $op $b" | bc -l)
-    printf "%s %s %s = %s\n\n" "$a" "$op" "$b" "$ans"
-break
+        read -p "Say me the first number?" a
+        if [[ $a =~ ^[0-9]+$ ]]; then
+                echo "${a} is a number"
+        else
+                echo "${a} is not a number"
+                break
+        fi
+        read -p "Say me the second number?" b
+        if [[ $b =~ ^[0-9]+$ ]]; then
+                echo "${b} is a number"
+        else
+                echo "${b} is not a number"
+                break
+        fi
+        PS3="What kind of operation do you want?"
+        select ans in add subtract multiply divide; do
+                case $ans in
+                        add) op='+' ; break ;;
+                        subtract) op='-' ; break ;;
+                        multiply) op='*' ; break ;;
+                        divide) op='/' ; break ;;
+                        *) echo "invalid response" ;;
+                esac
+        done
+        ans=$(echo "$a $op $b" | bc -l)
+        printf "%s %s %s = %s\n\n" "$a" "$op" "$b" "$ans"
+        break
 done
