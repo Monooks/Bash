cb97a2c6f04acd87453db87300a8ae06f097bf93
Monooks <124846515+Monooks@users.noreply.github.com>
2023-02-28 14:32:20 +0700

    Update time.sh

--- time.sh
+++ time.sh
@@ -1,9 +1,9 @@
 #!/bin/bash
 while true; do
- current_uptime () {
-        uptime
+ current_time () {
+        date +%T
  }
- current_uptime
+ current_time
  cat /proc/loadavg
   sleep 5
   if [[ $(($RANDOM%10)) -eq 5 ]]; then
